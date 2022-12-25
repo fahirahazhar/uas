669006429b9fff14178c194340d81ba66c80f7ad
msopiann <muhammadsopian452@gmail.com>
2022-12-25 19:53:02 +0700

    Fix pro

--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Pertama Buka
 Route::get('/', function () {
-    return view('home', ['title' => 'home']);
-})->name('home');
+    return view('user.register', ['title' => 'register']);
+})->name('register');
+Route::get('register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class, 'register_action'])->name('register.action');
 
-Route::get('/', function () {
+// Buka Kedua
+Route::get('/login', function () {
+    return view('login', ['title' => 'login']);
+})->name('login');
+Route::get('login', [UserController::class, 'login'])->name('login');
+Route::post('login', [UserController::class, 'login_action'])->name('login.action');
+
+// Buka Ketiga
+Route::get('/homepage', function () {
     return view('welcome', ['title' => 'welcome']);
 })->name('welcome');
 
-Route::get('/', function () {
-    return view('login', ['title' => 'login']);
-})->name('login');
-
-Route::get('/', function () {
-    return view('register', ['title' => 'register']);
-})->name('register');
+// Opsional
+Route::get('/home', function () {
+    return view('home', ['title' => 'home']);
+})->name('home');
 
 
-Route::get('register', [UserController::class, 'register'])->name('register');
-Route::post('register', [UserController::class, 'register_action'])->name('register.action');
-Route::get('login', [UserController::class, 'login'])->name('login');
-Route::post('login', [UserController::class, 'login_action'])->name('login.action');
 Route::get('password', [UserController::class, 'password'])->name('password');
 Route::post('password', [UserController::class, 'password_action'])->name('password.action');
+
 Route::get('logout', [UserController::class, 'logout'])->name('logout');
+Route::post('logout', [UserController::class, 'logout'])->name('logout');
